91723b4878ad52c33bf554fb5546bcfa9b141b8b
kitopang <kitop40@gmail.com>
2018-12-12 18:10:51 -0800

    Add files via upload

---
+++ Temp Reading.py
@@ -0,0 +1,24 @@
+from Tkinter import *
+import time
+import datetime
+import Adafruit_DHT
+
+window = Tk()
+window.title("Temperature Reading")
+window.geometry('480x320')
+sensor = 22
+pin = 4
+
+def callback():
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    temp = int(temperature) * 9/5.0 +32
+    time = datetime.datetime.now()
+    lbl = Label(window, text=temp, font=("Arial Bold", 20))
+    lbl.pack()
+    lbl2 = Label(window, text = time.strftime("%r") + "     (In Farenheit above)", font=("Arial Bold", 15))
+    lbl2.pack()
+    
+b = Button(window, text = "Refresh", command=callback)
+b.pack()
+          
+window.mainloop()
